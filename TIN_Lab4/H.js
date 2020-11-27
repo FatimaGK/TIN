54adfb356972b0665832dba389596a8b24cf74bb
Fatima Gul Kus <47695414+FatimaGK@users.noreply.github.com>
2020-11-27 18:02:06 +0100

    F

---
+++ TIN_Lab4/H.js
@@ -0,0 +1,40 @@
+// h)	Write a JavaScript function which will take an array of numbers stored and find the second lowest and second greatest numbers, respectively.
+function Second_Greatest_Lowest(arr)
+
+{
+ 
+  arr.sort(function(x,y)
+         
+           {
+        
+           return x-y;
+        
+           });
+ 
+  var value = [arr[0]];
+  
+
+  var result = [];
+  
+  for(var j=1; j < arr.length; j++)
+  
+  {
+  
+    if(arr[j-1] !== arr[j])
+  
+    {
+  
+      value.push(arr[j]);
+  
+    }
+  
+  }
+  
+    result.push(value[1],value[value.length-2]);
+  
+  return result.join(',');
+  
+  }
+
+
+console.log(Second_Greatest_Lowest([9,10,12,13,45,69]));
