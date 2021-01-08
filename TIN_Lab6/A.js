7626307158b83015721f4013ad3e6d88707cfe67
Fatima Gul Kus <47695414+FatimaGK@users.noreply.github.com>
2021-01-08 11:37:04 +0100

    6

---
+++ TIN_Lab6/A.js
@@ -0,0 +1,3 @@
+//Write  a function converting temprature between Celcius to Fahrenheight scales 
+//taking input from form of page and printing out in same page 
+//Function shoul dbe invoked by HTML button
