39dcb68d29dc3ced577700223e962f871311c01b
Fatima Gul Kus <47695414+FatimaGK@users.noreply.github.com>
2021-01-08 12:23:27 +0100

    done

--- TIN_Lab6/b.js
+++ TIN_Lab6/b.js
@@ -1,8 +1,7 @@
 // b)	Write a JS function that will add a paragraph of text to the page after 5 seconds from opening the page
-var myVar=setInterval(function(){myTimer()},5000);
+var myVar = setInterval(function () { myTimer() }, 5000);
 
-function myTimer()
-{
-var t="This is a Paragraph.";
-document.getElementById("p").innerHTML=t;
+function myTimer() {
+    var t = "This is a Paragraph.";
+    document.getElementById("p2").innerHTML = "this is a text that has been added after 5 second";
 }
