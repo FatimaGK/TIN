6b5e33c1fcb23462df1f5a8cfefec38f04cc22d5
Fatima Gul Kus <47695414+FatimaGK@users.noreply.github.com>
2021-01-08 15:20:02 +0100

    done

--- TIN_Lab6/A.js
+++ TIN_Lab6/A.js
@@ -5,6 +5,5 @@ function temperatureConverter() {
     event.preventDefault(); //stops from refreshing
     var celsiusValue = document.getElementById("celsiusText").value;
     var fahrenheit = celsiusValue * 1.8 + 32;
-    //document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
     document.getElementById("task").innerHTML = celsiusValue + " Celsius = " + fahrenheit + " Fahrenheit";
   }
