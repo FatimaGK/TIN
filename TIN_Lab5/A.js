6f8cda5bb866a379e46c5a80939ae85cdedf792c
Fatima Gul Kus <47695414+FatimaGK@users.noreply.github.com>
2021-01-08 11:22:48 +0100

    a.

--- TIN_Lab5/A.js
+++ TIN_Lab5/A.js
@@ -1,1 +1,22 @@
 // Use object initializer to create an object, it should have at least 2-3 fields and 2-3 methods. Write a function accepting the object as a parameter and printing all its properties and their types
+let person = {
+    name: 'John',
+    surname: 'Smith',
+    age: 35,
+    greet() {
+        console.log('Hello!');
+    },
+    goodbye() {
+        console.log('Bye!')
+    }
+};
+
+function show(obj)
+{
+    for(let propt in obj)
+    {
+        console.log(propt + ': ' + obj[propt]);
+    }
+}
+
+show(person);
