3f46e8ce2c59d2a563105336da28d5c1fad4103c
Fatima Gul Kus <47695414+FatimaGK@users.noreply.github.com>
2021-01-08 11:39:01 +0100

    tasks

---
+++ TIN_Lab6/D.js
@@ -0,0 +1,2 @@
+// d)	Write a JS function that will add a row to HTML table
+// based on contents of a form on the same page. Function should be invoked using HTML button.
---
+++ TIN_Lab6/b.js
@@ -0,0 +1,1 @@
+// b)	Write a JS function that will add a paragraph of text to the page after 5 seconds from opening the page
