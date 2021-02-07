43f03785c4bb1e06aa67ba14d9def83360426e9f
Fatima Gul Kus <47695414+FatimaGK@users.noreply.github.com>
2021-02-07 12:40:02 +0100

    Update A.js

--- TIN_Lab6/A.js
+++ TIN_Lab6/A.js
@@ -1,6 +1,4 @@
-//Write  a function converting temprature between Celcius to Fahrenheight scales 
-//taking input from form of page and printing out in same page 
-//Function shoul dbe invoked by HTML button
+
 function temperatureConverter() {
     event.preventDefault(); //stops from refreshing
     var celsiusValue = document.getElementById("celsiusText").value;
